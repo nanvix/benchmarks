993e28d39c91686e57d3520547188ebc1be9a641
João Fellipe <felippe.uller@gmail.com>
2021-02-17 18:20:16 -0300

    [capbench] Reworked IPC Statistics to Be Thread Independent

--- src/capbench/common/ipc.c
+++ src/capbench/common/ipc.c
@@ -31,63 +31,142 @@
 #include <mpi/datatype.h>
 #include <mpi.h>
 
-int rank;        /* Process Rank        */
-MPI_Group group; /* Communication Group */
+PRIVATE struct
+{
+	/* Timing statistics. */
+	uint64_t communication; /* Time spent on communication. */
+	uint64_t total;         /* Total time.                  */
 
-/* Timing statistics. */
-uint64_t master = 0;              /* Time spent on master.        */
-uint64_t spawn = 0;               /* Time spent spawning slaves   */
-uint64_t slave[MPI_PROCESSES_NR]; /* Time spent on slaves.        */
-uint64_t communication = 0;       /* Time spent on communication. */
-uint64_t total = 0;               /* Total time.                  */
+	/* Data exchange statistics. */
+	size_t data_sent;     /* Number of bytes received. */
+	unsigned nsend;       /* Number of sends.          */
+	size_t data_received; /* Number of bytes sent.     */
+	unsigned nreceive;    /* Number of receives.       */
+} _statistics[MPI_PROCS_PER_CLUSTER_MAX] = {
+	[0 ... (MPI_PROCS_PER_CLUSTER_MAX - 1)] = {0, 0, 0, 0, 0, 0}
+};
 
-/* Data exchange statistics. */
-size_t data_sent = 0;     /* Number of bytes received. */
-unsigned nsend = 0;       /* Number of sends.          */
-size_t data_received = 0; /* Number of bytes sent.     */
-unsigned nreceive = 0;    /* Number of receives.       */
+/*============================================================================*
+ * Statistics                                                                 *
+ *============================================================================*/
 
-/*
- * Sends data.
+/**
+ * @brief Retrieves total time.
+ */
+PUBLIC uint64_t total()
+{
+	return (_statistics[curr_mpi_proc_index()].total);
+}
+
+/**
+ * @brief Updates total time.
+ */
+PUBLIC void update_total(uint64_t amnt)
+{
+	_statistics[curr_mpi_proc_index()].total += amnt;
+}
+
+/**
+ * @brief Retrieves communication time.
+ */
+PUBLIC uint64_t communication()
+{
+	return (_statistics[curr_mpi_proc_index()].communication);
+}
+
+/**
+ * @brief Updates communication time.
+ */
+PUBLIC void update_communication(uint64_t amnt)
+{
+	_statistics[curr_mpi_proc_index()].communication += amnt;
+}
+
+/**
+ * @brief Retrieves amount of data sent.
+ */
+PUBLIC size_t data_sent()
+{
+	return (_statistics[curr_mpi_proc_index()].data_sent);
+}
+
+/**
+ * @brief Retrieves amount of data received.
+ */
+PUBLIC size_t data_received()
+{
+	return (_statistics[curr_mpi_proc_index()].data_received);
+}
+
+/**
+ * @brief Retrieves number of sends performed.
+ */
+PUBLIC unsigned nsend()
+{
+	return (_statistics[curr_mpi_proc_index()].nsend);
+}
+
+/**
+ * @brief Retrieves number of receives performed.
+ */
+PUBLIC unsigned nreceive()
+{
+	return (_statistics[curr_mpi_proc_index()].nreceive);
+}
+
+/*============================================================================*
+ * Communication                                                              *
+ *============================================================================*/
+
+/**
+ * @brief Sends data.
  */
 uint64_t data_send(int outfd, void *data, size_t n)
 {
-	nsend++;
-	data_sent += n;
+	int local_index;
+
+	local_index = curr_mpi_proc_index();
+
+	_statistics[local_index].nsend++;
+	_statistics[local_index].data_sent += n;
 
 	perf_start(0, PERF_CYCLES);
-	MPI_Send(
-		data,
-		n,
-		MPI_BYTE,
-		outfd,
-		0,
-		MPI_COMM_WORLD
-	);
-	communication += perf_read(0);
+	uassert(MPI_Send(
+			data,
+			n,
+			MPI_BYTE,
+			outfd,
+			0,
+			MPI_COMM_WORLD
+		) == 0);
+	_statistics[local_index].communication += perf_read(0);
 
 	return (0);
 }
 
-/*
- * Receives data.
+/**
+ * @brief Receives data.
  */
 uint64_t data_receive(int infd, void *data, size_t n)
 {
-	nreceive++;
-	data_received += n;
+	int local_index;
+
+	local_index = curr_mpi_proc_index();
+
+	_statistics[local_index].nreceive++;
+	_statistics[local_index].data_received += n;
 
 	perf_start(0, PERF_CYCLES);
-	MPI_Recv(
-		data,
-		n,
-		MPI_BYTE,
-		infd,
-		0,
-		MPI_COMM_WORLD,
-		MPI_STATUS_IGNORE
-	);
-	communication += perf_read(0);
+	uassert(MPI_Recv(
+			data,
+			n,
+			MPI_BYTE,
+			infd,
+			0,
+			MPI_COMM_WORLD,
+			MPI_STATUS_IGNORE
+		) == 0);
+	_statistics[local_index].communication += perf_read(0);
 
 	return (0);
 }
